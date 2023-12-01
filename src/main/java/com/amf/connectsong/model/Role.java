23a50fc8d41b944026afb6af22ce7543c9fe4c66
airtonm4 <airton.atmi@gmail.com>
2023-12-01 11:18:34 -0300

    Added role table

---
+++ src/main/java/com/amf/connectsong/model/Role.java
@@ -0,0 +1,30 @@
+package com.amf.connectsong.model;
+
+import java.util.ArrayList;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private ArrayList<User> users;
+
+}
