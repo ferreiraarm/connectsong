049ece15aa0b25f296d3bd2080f46d11078920ba
airtonm4 <airton.atmi@gmail.com>
2023-12-02 11:47:21 -0300

    Sending spotify service to controll conflict.

---
+++ src/main/java/com/amf/connectsong/service/SpotifyService.java
@@ -0,0 +1,15 @@
+package com.amf.connectsong.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpotifyService {
+
+    public SpotifyService() {
+    }
+
+    public ResponseEntity<?> login() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
