c9c00e6165204bc99aa10214e59c3b91f8940e71
airtonm4 <airton.atmi@gmail.com>
2023-12-02 18:48:19 -0300

    Minor fixes to auth functions

--- src/main/java/com/amf/connectsong/controller/AuthController.java
+++ src/main/java/com/amf/connectsong/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amf.connectsong.config.jwt.JwtResponse;
 import com.amf.connectsong.dto.LoginDTO;
 import com.amf.connectsong.dto.SignupDTO;
 import com.amf.connectsong.service.AuthService;
@@ -16,6 +17,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -23,12 +25,12 @@ import jakarta.validation.Valid;
 @Tag(name = "ConnectSong")
 public class AuthController {
     @Autowired
-   private  AuthService authService;
+    private AuthService authService;
 
     AuthController() {
     }
 
-    @Operation(summary="Realiza Operações de validação de erro login", method="POST")
+    @Operation(summary = "Realiza Operações de validação de erro login", method = "POST")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Upload de arquivo realizado com sucesso"),
             @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
@@ -36,9 +38,17 @@ public class AuthController {
             @ApiResponse(responseCode = "500", description = "Erro ao realizar o upload de arquivo"),
     })
     @PostMapping("")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginDTO loginRequest) {
+    public ResponseEntity<?> login(@Valid @RequestBody LoginDTO loginRequest, HttpServletResponse response) {
         try {
-            return authService.login(loginRequest);
+            JwtResponse login = authService.login(loginRequest);
+            /**
+             * Testing if the token is being sent in the header
+             */
+            // response.setHeader("Authorization", "Bearer " + login.getAccessToken());
+            // Cookie cookie = new Cookie("Authorization", login.getAccessToken());
+            // response.addCookie(cookie);
+
+            return ResponseEntity.ok(login.getAccessToken());
         } catch (Exception e) {
             if (e.getMessage() == "USER_NOT_FOUND") {
                 return ResponseEntity.status(404).body("Error: User not found!");
@@ -52,9 +62,7 @@ public class AuthController {
         }
     }
 
-
-    
-    @Operation(summary="Realiza Operações Para o login", method="POST")
+    @Operation(summary = "Realiza Operações Para o login", method = "POST")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Upload de arquivo realizado com sucesso"),
             @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
