ce60bf3c108dbb9cc76de88eff1786c42f73141c
ArturMartinsFerreira <artur.ferreira@estudante.ifgoiano.edu.br>
2023-12-02 11:16:35 -0300

    inicio swagger

--- src/main/java/com/amf/connectsong/ConnectSongApplication.java
+++ src/main/java/com/amf/connectsong/ConnectSongApplication.java
@@ -3,7 +3,11 @@ package com.amf.connectsong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ConnectSong", version = "1", description = "API desenvolvida para escolha de albuns de musica no Spotify"))
 public class ConnectSongApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/amf/connectsong/controller/AuthController.java
+++ src/main/java/com/amf/connectsong/controller/AuthController.java
@@ -11,17 +11,29 @@ import com.amf.connectsong.dto.LoginDTO;
 import com.amf.connectsong.dto.SignupDTO;
 import com.amf.connectsong.service.AuthService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "ConnectSong")
 public class AuthController {
     @Autowired
-    AuthService authService;
+   private  AuthService authService;
 
     AuthController() {
     }
 
+    @Operation(summary="Realiza Operações de validação de erro login", method="POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Upload de arquivo realizado com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar o upload de arquivo"),
+    })
     @PostMapping("")
     String login(@Valid @RequestBody LoginDTO loginRequest) {
         try {
@@ -31,6 +43,15 @@ public class AuthController {
         }
     }
 
+
+    
+    @Operation(summary="Realiza Operações para o login", method="POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Upload de arquivo realizado com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar o upload de arquivo"),
+    })
     @PostMapping("/user")
     public ResponseEntity<?> signUp(@Valid @RequestBody SignupDTO singUpRequest) {
         try {
