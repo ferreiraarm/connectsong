9ce28748ef63a7adb5658fa109989d90a84a64be
airtonm4 <airton.atmi@gmail.com>
2023-12-03 12:03:23 -0300

    Added function to get the random album.

--- src/main/java/com/amf/connectsong/controller/RouletteController.java
+++ src/main/java/com/amf/connectsong/controller/RouletteController.java
@@ -1,53 +1,58 @@
 package com.amf.connectsong.controller;
 
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amf.connectsong.dto.AlbumDTO;
 import com.amf.connectsong.model.Roulette;
-import com.amf.connectsong.model.User;
 import com.amf.connectsong.repository.RouletteRepository;
+import com.amf.connectsong.service.RouletteService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.Delegate;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 
 @RestController
 @RequestMapping("/api/roulette")
 public class RouletteController {
-    
+
     @Autowired
     private RouletteRepository repRouletteObj;
 
-    @Operation(summary = "Busca todas as roletas", method = "GET")
+    @Autowired
+    private RouletteService rouletteService;
 
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
-            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
-            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
-            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados"),
-    })
-    @GetMapping("/api/roulette")
-    public List<Roulette> selecionarTodasAsRoulettes(){
-        return repRouletteObj.findAll();
-    }
+    // @Operation(summary = "Busca todas as roletas", method = "GET")
 
-    /* 
-    @GetMapping("/api/roulette/{User}")
-    public Roulette selecionarRoulettePorUser(@PathVariable User user){
+    // @ApiResponses(value = {
+    // @ApiResponse(responseCode = "200", description = "Busca realizada com
+    // sucesso"),
+    // @ApiResponse(responseCode = "422", description = "Dados de requisição
+    // inválida"),
+    // @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+    // @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos
+    // dados"),
+    // })
+    // @GetMapping("/api/roulette")
+    // public List<Roulette> selecionarTodasAsRoulettes(){
+    // return repRouletteObj.findAll();
+    // }
 
-        return repRouletteObj.findByUser(user);
-    }*/
+    /*
+     * @GetMapping("/api/roulette/{User}")
+     * public Roulette selecionarRoulettePorUser(@PathVariable User user){
+     * 
+     * return repRouletteObj.findByUser(user);
+     * }
+     */
 
     @Operation(summary = "Busca roletas por id", method = "GET")
-
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
             @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
@@ -55,10 +60,22 @@ public class RouletteController {
             @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados"),
     })
     @GetMapping("/api/roulette/{id}")
-    public Roulette selecionarRoulettePorId(@PathVariable long id){
+    public Roulette selecionarRoulettePorId(@PathVariable long id) {
         return repRouletteObj.findById(id);
     }
 
-    
+    @Operation(summary = "Gira a roleta", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados"),
+    })
+    @GetMapping("/api/roulette/spin")
+    public ResponseEntity<AlbumDTO> spinRoulette(@RequestHeader("Authorization") String token) {
+
+        return rouletteService.spinRoulette(token);
+        // return repRouletteObj.spinRoulette();
+    }
 
 }
--- src/main/java/com/amf/connectsong/service/RouletteService.java
+++ src/main/java/com/amf/connectsong/service/RouletteService.java
@@ -1,8 +1,67 @@
 package com.amf.connectsong.service;
 
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.amf.connectsong.config.jwt.JwtUtils;
+import com.amf.connectsong.dto.AlbumDTO;
+import com.amf.connectsong.model.Album;
+import com.amf.connectsong.model.Roulette;
+import com.amf.connectsong.model.User;
+import com.amf.connectsong.repository.RouletteRepository;
+import com.amf.connectsong.repository.UserRepository;
 
 @Service
 public class RouletteService {
 
+    @Autowired
+    private RouletteRepository rouletteRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    public ResponseEntity<AlbumDTO> spinRoulette(String token) {
+
+        if (token == null) {
+            throw new RuntimeException("TOKEN_NOT_FOUND");
+        }
+
+        String username = jwtUtils.getUserNameFromJwtToken(token);
+
+        Optional<User> currentUser = userRepository.findByUsername(username);
+
+        if (!currentUser.isPresent()) {
+            throw new RuntimeException("USER_NOT_FOUND");
+        }
+
+        Roulette roulette = rouletteRepository.findByUser(currentUser.get());
+
+        if (roulette == null) {
+            throw new RuntimeException("ROULETTE_NOT_FOUND");
+        }
+
+        Album[] rouletteAlbums = roulette.getAlbums().toArray(Album[]::new);
+
+        Album selectAlbum = getRandomAlbum(rouletteAlbums);
+
+        return ResponseEntity.ok(new AlbumDTO(selectAlbum));
+    }
+
+    private Album getRandomAlbum(Album[] rouletteAlbums) {
+        if (rouletteAlbums == null || rouletteAlbums.length == 0) {
+            throw new RuntimeException("NO_ALBUMS_FOUND");
+        }
+
+        int randomIndex = new Random().nextInt(rouletteAlbums.length);
+        return rouletteAlbums[randomIndex];
+    }
 }
